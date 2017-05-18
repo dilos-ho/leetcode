5182c13de050a8b6cb857ae13b8f0bc495baab08
Ian Ho <ho.ian96@gmail.com>
2017-05-17 17:26:46 -0700

    Complete Two Sum

--- twosum.py
+++ twosum.py
@@ -1,3 +1,8 @@
 class Solution:
     def twoSum(self, nums, target):
-        
+        neednum = {}
+        for i in range(len(nums)):
+            if (nums[i] in neednum):
+                return [neednum[nums[i]],i]
+            else:
+                neednum[target - nums[i]] = i
